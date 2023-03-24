8afb9a9669d6e7ae18131ba4f60e022a41214f29
banukayapa <it21002342@my.sliit.lk>
2023-03-24 14:32:06 +0530

    creating doctor side

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,30 @@
-console.log("Hello world");
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema;
+
+const DoctorsSchema = new schema({
+    doctorId :{
+        type : Number,
+        required : true
+    },
+
+    doctorName :{
+        type : String,
+        required : true
+    },
+
+    doctorAge :{
+        type : String,
+        required : true
+    },
+
+    doctorSpecialization :{
+        type : String,
+        required : true
+    }
+
+})
+
+const Doctors = mongoose.model("Doctors",salesSchema);
+
+module.exports = Doctors;
